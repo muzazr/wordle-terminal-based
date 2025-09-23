53b0d67b67d9e00b0ff934bca8eebe5a8302390c
Muhammad Zaki Azhar <149247787+muzazr@users.noreply.github.com>
2025-09-23 14:26:17 +0700

    Menambahkan fitur mengubah bahasa

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <iomanip>
 #include "../include/game.hpp"
 #include "../include/utils.hpp"
 
@@ -10,14 +11,39 @@ using namespace std;
 int main() {
     srand(time(0));
 
-    vector<string> secretWords = loadWords("../src/words.txt");
-    bool play = true; //to give players the option to play again, true for play again
+    cout << setfill('=') << setw(31) << '=' << endl;
+    cout << "   << WORDLE (C++ Edition) >>   " << endl; 
+    cout << setfill('=') << setw(31) << '=' << endl;
+    cout << "   Guess the 5-letter word!   " << endl << endl;
 
-    while (play) {
-        playGame(secretWords);
-        cout << "Want to play again? (y/n) : ";
-        char playAgain;
-        cin >> playAgain;
-        play = (playAgain == 'y');
+    while(true) {
+        cout << "Choose language:\n(1) English\n(2) Bahasa Indonesia\nSelect\t\t\t: ";
+        char language; cin >> language;
+        
+        if(language == '1') {
+            vector<string> secretWords = loadWords("../src/wordsEnglish.txt");
+            bool play = true; //to give players the option to play again, true for play again
+            
+            while (play) {
+                playGameEnglish(secretWords);
+                cout << "Play again? (y/n) : ";
+                char playAgain;
+                cin >> playAgain;
+                play = (playAgain == 'y');
+            }
+            break;
+        } else if (language == '2') {
+            vector<string> secretWords = loadWords("../src/wordsBahasa.txt");
+            bool play = true; //to give players the option to play again, true for play again
+            
+            while (play) {
+                playGameBahasa(secretWords);
+                cout << "Ingin bermain lagi? (y/n) : ";
+                char playAgain;
+                cin >> playAgain;
+                play = (playAgain == 'y');
+            }
+            break;
+        }
     }
 }
